91405ed1d097f6126332dbc0adc191fb025c756e
Guto Maia <guto@guto.net>
2011-05-13 00:29:38 -0300

    start using the RequestContext to pass the request vars to the view template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def homepage(request):
-	from django.conf import settings
-	context = {'STATIC_URL':settings.STATIC_URL}
+	context = RequestContext(request)
 	return render_to_response('index.html', context)
