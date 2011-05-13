8b1c9170acaaf2f68e7bc03389007b46c3d9d842
Guto Maia <guto@guto.net>
2011-05-13 00:06:16 -0300

    first use of templates

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader, Context
 
 def homepage(request):
-	return HttpResponse('Bem-vindo ao Eventex!')
+	t = loader.get_template('index.html')
+	c = Context()
+	content = t.render(c)
+	return HttpResponse(content)
