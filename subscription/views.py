dca71f40e6558779086c2e518e4511c1e56a1790
Guto Maia <guto@guto.net>
2011-05-29 03:00:37 -0300

    dealing with the post in the subscription view

--- subscription/views.py
+++ subscription/views.py
@@ -1,5 +1,11 @@
 def subscribe(request):
-	form = SubscriptionForm()
+	if request.method == 'POST':
+		form = SubscriptionForm(request.POST)
+		if form.is_valid():
+			subscription = form.save()
+			return HttpResponseRedirect(reverse('subscription:success', args=[subscription.pk]))
+	else:
+		form = SubscriptionForm()
 
 	context = RequestContext(request, {'form': form})
 	return render_to_response('subscription/new.html', context)
