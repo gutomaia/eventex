0d8a83be0192c482f7ecc7fb2d5e82dfc9caff41
Guto Maia <guto@guto.net>
2011-05-29 03:06:23 -0300

    refactoring the subscription views. Extract methods new and create from subscribe

--- subscription/views.py
+++ subscription/views.py
@@ -1,11 +1,21 @@
-def subscribe(request):
-	if request.method == 'POST':
-		form = SubscriptionForm(request.POST)
-		if form.is_valid():
-			subscription = form.save()
-			return HttpResponseRedirect(reverse('subscription:success', args=[subscription.pk]))
-	else:
-		form = SubscriptionForm()
-
+def new(request):
+	form = SubscriptionForm()
 	context = RequestContext(request, {'form': form})
 	return render_to_response('subscription/new.html', context)
+
+
+def create(request):
+	form = SubscriptionForm(request.POST)
+	if not valid():
+		context = RequestContext(request, {'form': form})
+		return render_to_response('subscription/new.html', context)
+
+	subscription = form.save()
+	return HttpResponseRedirect(reverse('subscription:success', args=[subscription.pk]))
+
+
+def subscribe(request):
+	if request.method == 'POST':
+		return create(request)
+	else:
+		return new(request)
