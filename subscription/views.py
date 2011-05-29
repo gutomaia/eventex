80368548cf633cc60b10d1b3d0e6d5dd4ffcc9e4
Guto Maia <guto@guto.net>
2011-05-29 02:44:52 -0300

    first view method on the subscription view and also the template for the corresponding method

--- subscription/views.py
+++ subscription/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+def subscribe(request):
+	form = SubscriptionForm()
+
+	context = RequestContext(request, {'form': form})
+	return render_to_response('subscription/new.html', context)
